dca2bf16ba938c2baa976b3e768b551866755a80
Javier <javito81@hotmail.com>
2026-09-04 17:52:05 -0300

    Publicacion automatica - version 04/09 17:48

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
  *    el dibujo cambia el nombre o la version del cache. Asi una publicacion
  *    nueva no obliga a bajar de nuevo 268 KB de arte que no cambio.
  */
-var VERSION = '04/09 17:27';
+var VERSION = '04/09 17:48';
 var CACHE = 'apuntes-' + VERSION;
 
 var ESENCIALES = ['./', './index.html', './manifest.json',
